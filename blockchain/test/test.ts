8a1210939c6bd9296cc8b75dff7c0d094883c709
KaiStryker <kareemj.williams@gmail.com>
2022-10-08 11:17:42 -0500

    set up environment

---
+++ blockchain/test/test.ts
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("", function () {
+  describe("", function () {
+    it("", async function () {});
+
+    it("", async function () {});
+
+    it("", async function () {});
+  });
+
+  describe("", function () {
+    describe("", function () {
+      it("", async function () {});
+
+      it("", async function () {});
+
+      it("", async function () {});
+    });
+
+    describe("", function () {
+      it("Should emit an event on withdrawals", async function () {});
+    });
+
+    describe("", function () {
+      it("", async function () {});
+    });
+  });
+});
